d17b3912d1dac9658f6397c809ed235785e37ede
Palak-Agrawal <palak1998agrawal@gmail.com>
2018-10-07 14:18:44 +0530

    trying to create session

--- patient/home.aspx.cs
+++ patient/home.aspx.cs
@@ -9,6 +9,6 @@ public partial class patient_home : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        string check1 = Session["UserID"].ToString();
     }
 }
